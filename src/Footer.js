b3966be5e5738977f34fb0cd58372bdd03bacf2b
Robert McAbee <robertthemcabee@gmail.com>
2022-07-07 19:23:34 -0600

    update url and metadata

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,17 @@
 const Footer = () => {
-  return ( 
+  return (
     <footer className="bg-white h-full text-cyan-800 flex align-middle justify-center">
-      <p className="p-8 text-lg opacity-75">Created by <a href="https://robertmcabee.netlify.app" className="underline decoration-cyan-500 decoration-2 underline-offset-2 hover:text-cyan-500 transition duration-200 ease-in-out">
-        Robert McAbee
-      </a></p>
+      <p className="p-8 text-lg opacity-75">
+        Created by{" "}
+        <a
+          href="https://robertmcabee.com"
+          className="underline decoration-cyan-500 decoration-2 underline-offset-2 hover:text-cyan-500 transition duration-200 ease-in-out"
+        >
+          Robert McAbee
+        </a>
+      </p>
     </footer>
-   );
-}
- 
+  );
+};
+
 export default Footer;
